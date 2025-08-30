c528b7f33396be363a57d1e8c0bfadcccc4c2d52
Jackson-DKMG <65887967+Jackson-DKMG@users.noreply.github.com>
2025-08-30 09:29:20 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,16 +7,11 @@ const config = {
   tagline: 'High Stakes Switzerland – Professional staking services hosted in Switzerland.',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
   url: 'https://docs.highstakes.ch/',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'High Stakes Switzerland', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+  organizationName: 'High Stakes Switzerland',
+  projectName: 'docs',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -24,8 +19,7 @@ const config = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'fr'],
-    localeConfigs: {
-    },
+    localeConfigs: {},
   },
 
   presets: [
@@ -47,9 +41,10 @@ const config = {
       }
     ],
   ],
-/**open links in a new tab by default*/
-    markdown: {
-    externalLinks: {
+
+  // Moved externalLinks config into customFields
+  customFields: {
+    markdownExternalLinks: {
       target: '_blank',
       rel: ['noopener', 'noreferrer'],
     },
@@ -57,104 +52,64 @@ const config = {
 
   themeConfig:
   /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-      {
-        navbar: {
+  {
+    navbar: {
+      items: [
+        { type: 'localeDropdown', position: 'left' },
+        {
+          type: 'html',
+          value:
+            '<a class="navbar__brand" href="/"><div class="navbar__logo"><img src="/img/hss-logo.png" alt="High Stakes Switzerland Logo"></div>High Stakes 🇨🇭 Docs</a>'
+        },
+        {
+          href: 'https://github.com/HighStakesSwitzerland/',
+          position: 'right',
+          className: 'header-github-link',
+          'aria-label': 'GitHub repository'
+        },
+        { type: 'docsVersionDropdown', position: 'right' }
+      ]
+    },
+    footer: {
+      style: 'light',
+      links: [
+        {
+          title: 'Docs',
           items: [
-            {
-              type: 'localeDropdown',
-              position: 'left',
-            },
-            {
-              type: 'html',
-              value:
-                  '<a class="navbar__brand" href="/"><div class="navbar__logo"><img src="/img/hss-logo.png" alt="High Stakes Switzerland Logo"></div>High Stakes 🇨🇭 Docs</a>'
-            },
-            {
-              href: 'https://github.com/HighStakesSwitzerland/',
-              position: 'right',
-              className: 'header-github-link',
-              'aria-label': 'GitHub repository'
-            },
-            {
-              type: 'docsVersionDropdown',
-              position: 'right'
-            }
+            { label: 'Loyalty Program', to: '/loyalty-program/' },
+            { label: 'Stellee', to: '/stellee/' },
+            { label: 'Staking Guides', to: '/delegating/terra' }
           ]
         },
-        footer: {
-          style: 'light',
-          links: [
-            {
-              title: 'Docs',
-              items: [
-                {
-                  label: 'Loyalty Program',
-                  to: '/loyalty-program/'
-                },
-                {
-                  label: 'Stellee',
-                  to: '/stellee/'
-                },
-                {
-                  label: 'Staking Guides',
-                  to: '/delegating/terra'
-                }
-              ]
-            },
-            {
-              title: 'Community',
-              items: [
-                {
-                  label: 'Discord',
-                  href: 'https://discord.gg/W6hDHTVn7r'
-                },
-                {
-                  label: 'Twitter',
-                  href: 'https://twitter.com/HighStakes_CH'
-                },
-                {
-                  label: 'Telegram',
-                  href: 'https://t.me/HighStakesSwitzerland'
-                }
-              ]
-            },
-            {
-              title: 'More',
-              items: [
-                {
-                  label: 'Website',
-                  href: 'https://highstakes.ch/'
-                },
-                {
-                  label: 'GitHub',
-                  href: 'https://github.com/HighStakesSwitzerland/docs/'
-                }
-              ]
-            }
-          ],
-          copyright: `Copyright © ${new Date().getFullYear()} High Stakes 🇨🇭`
+        {
+          title: 'Community',
+          items: [
+            { label: 'Discord', href: 'https://discord.gg/W6hDHTVn7r' },
+            { label: 'Twitter', href: 'https://twitter.com/HighStakes_CH' },
+            { label: 'Telegram', href: 'https://t.me/HighStakesSwitzerland' }
+          ]
         },
-        colorMode: {
-          defaultMode: 'light',
-          disableSwitch: false,
-          respectPrefersColorScheme: true
-        },
-        prism: {
-          theme: require('prism-react-renderer').themes.github,
-          darkTheme: require('prism-react-renderer').themes.dracula,
-        },
-        algolia: false,
-        /*
-        announcementBar: {
-          id: 'support_us',
-          content: '',
-          backgroundColor: '#3182ce',
-          textColor: '#fff',
-          isCloseable: true
+        {
+          title: 'More',
+          items: [
+            { label: 'Website', href: 'https://highstakes.ch/' },
+            { label: 'GitHub', href: 'https://github.com/HighStakesSwitzerland/docs/' }
+          ]
         }
-
-         */
-      },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} High Stakes 🇨🇭`
+    },
+    colorMode: {
+      defaultMode: 'light',
+      disableSwitch: false,
+      respectPrefersColorScheme: true
+    },
+    prism: {
+      theme: require('prism-react-renderer').themes.github,
+      darkTheme: require('prism-react-renderer').themes.dracula,
+    },
+    algolia: false,
+  },
 };
 
 module.exports = config;
